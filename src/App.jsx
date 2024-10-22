440788423d3a1e107ab5a95cbb1ea636a871ae08
mehedi hasan <mhshohan04@gmail.com>
2024-10-22 16:17:15 +0600

    project setup

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+import Header from './components/Header';
+
+function App() {
+
+  return (
+    <>
+      {/* <h2 className="text-red-500">Mehedi Hasan Shohan</h2> */}
+      <Header></Header>
+    </>
+  )
+}
+
+export default App
